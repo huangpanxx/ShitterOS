747302e94cd3f5688a98df63a253529ba8f5bd36
qqz003 <awake@awake-Inspiron-1464.(none)>
2013-12-12 15:46:19 +0800

    type.h port.h

---
+++ include/port.h
@@ -0,0 +1,5 @@
+#include "type.h"
+
+void out_byte(WORD port, BYTE value);
+
+BYTE in_byte(WORD port);
---
+++ include/type.h
@@ -0,0 +1,11 @@
+#ifndef BYTE
+#define BYTE unsinged char
+#endif
+
+#ifndef WORD
+#define WORD unsinged short
+#endif
+
+#ifndef DWORD
+#define DWORD unsinged int
+#endif
